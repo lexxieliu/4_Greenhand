ec54bd8fabc3f8a44630ff2695ccc0e2bc50b794
Afrillian Ramadhan <afrillianramadhan@wirelessprv-10-195-1-241.near.illinois.edu>
2026-09-17 20:15:02 -0500

    Deleting the comments

--- userprofile/models.py
+++ userprofile/models.py
@@ -2,12 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    """
-    Extends the built-in Django User model with additional personal details.
-    Stores customizable user attributes such as biography, birthday, and pronouns.
-    We use one to one field to make a relationship with the user model, and use cascade rule so if the user got deleted
-    the profile also deleted
-    """
+
     profile_id = models.AutoField(primary_key=True)
     bio = models.TextField(max_length=500, blank=True, default="")
     birthdate = models.DateField(null=True, blank=True)
