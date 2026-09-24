27a6bd4def87f0ad27fc730136f3f21e51b9addd
Lexxie Liu <xliu174@illinois.edu>
2026-09-24 17:31:22 -0500

    Add a GET search form to the Plants list

--- plants/views.py
+++ plants/views.py
@@ -12,6 +12,18 @@ class PlantListView(ListView):
     template_name = 'plant/plant_list.html'
     context_object_name = 'plants_list'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(plant_name__icontains=query)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q', '')
+        return context
+
 class PlantDetailView(View):
 
     def get(self, request, primary_key):
